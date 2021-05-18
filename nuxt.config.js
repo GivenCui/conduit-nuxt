16362082590feb81790bb52c88d39781a1789bde
GivenCui <wow952100544@163.com>
2021-05-19 05:53:44 +0800

    feat: nuxtjs 默认路由配置不满足需求, 配置 nuxt.config.js 覆盖默认路由配置

---
+++ nuxt.config.js
@@ -0,0 +1,33 @@
+/**
+ * Nuxt.js 配置文件
+ * https://zh.nuxtjs.org/docs/2.x/configuration-glossary/configuration-router/#extendroutes
+ */
+
+ export default {
+    router: {
+      extendRoutes(routes, resolve) {
+        console.log('----->', routes) // <-- console.log 在 控制台打印
+        // 清空 routes 数组, 清除 Nuxt.js 基于 pages 目录的 默认配置
+        routes.splice(0)
+
+        routes.push(...[
+            {
+                path: '/',
+                component: resolve(__dirname, 'pages/layout/'),
+                children: [
+                    {
+                        path: '', // 默认子路由
+                        name: 'home',
+                        component: resolve(__dirname, 'pages/home/')
+                    }
+                ]
+            }
+        ])
+        // routes.push({
+        //   name: 'custom',
+        //   path: '*',
+        //   component: resolve(__dirname, 'pages/404.vue')
+        // })
+      }
+    }
+  }
