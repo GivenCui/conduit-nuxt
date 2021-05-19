ac07d67bfdbad3c8ef404567b5ac0186f2f603d9
GivenCui <wow952100544@163.com>
2021-05-19 14:55:28 +0800

    feat: 7-处理顶部nav中的导航链接

--- pages/layout/index.vue
+++ pages/layout/index.vue
@@ -3,29 +3,39 @@
     <!-- 顶部导航栏 -->
     <nav class="navbar navbar-light">
       <div class="container">
-        <a class="navbar-brand"
-           href="index.html">conduit</a>
+        <!-- <a class="navbar-brand" href="index.html">conduit</a> -->
+        <nuxt-link class="navbar-brand"
+                   to="/">conduit</nuxt-link>
         <ul class="nav navbar-nav pull-xs-right">
           <li class="nav-item">
             <!-- Add "active" class when you're on that page" -->
-            <a class="nav-link active"
-               href="">Home</a>
+            <!-- <a class="nav-link active" href="">Home</a> -->
+            <nuxt-link class="nav-link active"
+                       to="">Home</nuxt-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link"
-               href="">
-              <i class="ion-compose"></i>&nbsp;New Post
-            </a>
+            <nuxt-link class="nav-link"
+                       to="/editor"><i class="ion-compose"></i>&nbsp;New Post</nuxt-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link"
-               href="">
-              <i class="ion-gear-a"></i>&nbsp;Settings
-            </a>
+            <nuxt-link class="nav-link"
+                       to="/settings"><i class="ion-gear-a"></i>&nbsp;Settings</nuxt-link>
           </li>
           <li class="nav-item">
-            <a class="nav-link"
-               href="">Sign up</a>
+            <nuxt-link class="nav-link"
+                       to="/login">Sign in</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link class="nav-link"
+                       to="/register">Sign up</nuxt-link>
+          </li>
+          <li class="nav-item">
+            <nuxt-link class="nav-link"
+                       to="/profile/1">
+              <img class="user-pic"
+                   src="https://avatars.githubusercontent.com/u/17002107?s=400&amp;u=e86d6529e37d7cd872b20005ebbe86be72e7a38e&amp;v=4" />
+              givencui
+            </nuxt-link>
           </li>
         </ul>
       </div>
@@ -37,7 +47,8 @@
     <!-- 底部导航栏 -->
     <footer>
       <div class="container">
-        <a href="/" class="logo-font">conduit</a>
+        <a href="/"
+           class="logo-font">conduit</a>
         <span class="attribution">
           An interactive learning project from <a href="https://thinkster.io">Thinkster</a>. Code &amp; design licensed under MIT.
         </span>
@@ -49,7 +60,7 @@
 
 <script>
 export default {
-    name: 'LayoutIndex'
+  name: 'LayoutIndex'
 }
 </script>
 
